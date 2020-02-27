5e94a3a998a6c74a6fc21de23c5bdc6ee80c0105
Assylbek <assylbek@iMac-Assylbek.local>
2020-02-27 17:12:31 +0600

    1.0.0

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export const About = () => (
+  <div className="jumbotron">
+    <h1 className="display-4">React product</h1>
+    <p className="lead">
+      Version product <strong>1.0.04</strong>
+    </p>
+  </div>
+)
